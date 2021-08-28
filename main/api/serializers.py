51e5f756326a06bf8905f5d68e0de28ac21fdd59
k1oten <clotterberg@gmail.com>
2021-08-28 14:38:33 +0300

    Changed serializers.py

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from main.models import Project
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
+    thumbnail = serializers.ImageField(max_length=None, use_url=True)
     class Meta:
         model = Project
         fields = ('headline', 'link', 'thumbnail', 'color')
