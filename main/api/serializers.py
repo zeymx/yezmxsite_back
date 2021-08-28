7c347d705ffab59e4bfa22fd9643a2a4c07f43f5
k1oten <clotterberg@gmail.com>
2021-08-28 13:05:46 +0300

    Changed serializers.py and cleaned views.py

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from main.models import Project
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    thumbnail = serializers.ImageField(max_length = None, use_url=True)
     class Meta:
         model = Project
         fields = ('headline', 'link', 'thumbnail', 'color')
--- main/views.py
+++ main/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from .models import Project
-
-def main(request):
-
-    projects = Project.objects.all()
-
-    context = {'projects': projects}
-    return render(request, 'main/main.html', context)
