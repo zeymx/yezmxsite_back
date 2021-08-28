f038793d100b728b4d5085f8e0a6ea545e14d71e
k1oten <clotterberg@gmail.com>
2021-08-28 15:20:11 +0300

    Changed urls

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,4 @@ from rest_framework.authtoken import views as restviews
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-token-auth/', restviews.obtain_auth_token, name="api-token-auth")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
